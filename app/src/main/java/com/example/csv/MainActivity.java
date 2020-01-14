b5671b13165e3982e13553efb3f37c02aa892851
Hridoy <jubairuva@gmail.com>
2020-01-15 00:26:16 +0600

    Second Commit

--- app/src/main/java/com/example/csv/MainActivity.java
+++ app/src/main/java/com/example/csv/MainActivity.java
@@ -1,22 +1,30 @@
 package com.example.csv;
 import android.Manifest;
 import android.content.pm.PackageManager;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
+
+import org.apache.commons.beanutils.Converter;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -48,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
                     String Number1=Number.getText().toString();
                     File file =new File("/sdcard/hridoy");
                     file.mkdir();
-                    String csv="/sdcard/hridoy/d.csv";
+                    String csv="/sdcard/hridoy/e.csv";
                     CSVWriter csvWriter= null;
                     try {
                         csvWriter = new CSVWriter(new FileWriter(csv,true));
@@ -67,14 +75,50 @@ Next.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View v) {
         CSVReader reader = null;
-        try {
-            reader = new CSVReader(new FileReader("/sdcard/hridoy/d.csv"));
-            List myEntries = reader.readAll();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        CSVWriter csvWriter= null;
+        String[] row = null;
+        String[] FileEx=new String[]{"a","b","c","d"};
+       for(int i=0;i<=3;i++)
+       {
+           try {
+               reader = new CSVReader(new FileReader("/sdcard/hridoy/"+FileEx[i]+".csv"));
+               List myEntries = reader.readAll();
+               reader.close();
+
+               File file = new File("/sdcard/hridoy");
+               file.mkdir();
+               String csv = "/sdcard/hridoy/e.csv";
+               try {
+                   csvWriter = new CSVWriter(new FileWriter(csv, true));
+
+                   String element[] = null;
+                   // int i=0;
+                   //while((myEntries.get(i)) != null)
+                   {
+                       for (Object object : myEntries) {
+                           row = (String[]) object;
+
+                           csvWriter.writeNext(row);
+
+                       }
+
+                       csvWriter.close();
+                       reader.close();
+                       // i++;
+                       // Toast.makeText(MainActivity.this,   row[0]+""+myEntries.get(i)+""+reader.readNext()+" "+reader.peek(), Toast.LENGTH_LONG).show();
+                   }
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+
+
+           } catch (FileNotFoundException e) {
+               e.printStackTrace();
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+       }
+        Toast.makeText(MainActivity.this, "Data Added Successfully...", Toast.LENGTH_LONG).show();
 
     }
 });
